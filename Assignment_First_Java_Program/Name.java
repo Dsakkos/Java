33c40962be6d50c6bbefdb15532d74a7956c895e
Dsakkos <dmaster456@yahoo.com>
2022-07-04 17:09:26 -0400

    First Java Program
    Assignment First Java Program

---
+++ Assignment_First_Java_Program/Name.java
@@ -0,0 +1,7 @@
+public class Name {
+    public static void main(String[] args) {
+        System.out.println("My name is Dimitri Sakkos");
+        System.out.println("I am 27 years old");
+        System.out.println("My Hometown is Holliston, MA");
+    }
+}
