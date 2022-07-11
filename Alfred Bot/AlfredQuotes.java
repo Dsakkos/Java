c83a5c0e79d0dfafb46e83331624cb95a00f8857
Dsakkos <dmaster456@yahoo.com>
2022-07-11 16:56:29 -0400

    Alfred Bot
    Changed

--- Alfred Bot/AlfredQuotes.java
+++ Alfred Bot/AlfredQuotes.java
@@ -7,24 +7,32 @@ public class AlfredQuotes {
     }
 
     public String guestGreeting(String name) {
-        return "Hello, " + name + ". Lovely to see you.";
+        // return "Hello, " + name + ". Lovely to see you.";
+        return String.format("Hello %s. Lovely to see you.", name);
     }
 
     public String dateAnnouncement() {
-        Date date = new Date();
-        return "It is currently " + date;
+        // Date date = new Date();
+        // return "It is currently " + date;
+        return String.format("It is currently %s", new Date());
     }
 
     public String respondBeforeAlexis(String conversation) {
-        String message = conversation;
-        if (message.contains("Alexis")) {
-        return "Right away, sir.She certainly isn't sophisticated enough for that";
+        // String message = conversation;
+        // if (message.contains("Alexis")) {
+        // return "Right away, sir.She certainly isn't sophisticated enough for that";
+        // }
+        // else if(message.contains("Alfred")){
+        // return "At your service. As you wish, naturally";
+        // }
+        // else {
+        // return "Right.And with that I shall Retire";
+        if(conversation.indexOf("Alexis") > -1) {
+            return "Right away, sir.She certainly isn't sophisticated enough for that";
         }
-        else if(message.contains("Alfred")){
-        return "At your service. As you wish, naturally";
+        if(conversation.indexOf("Alfred") > -1) {
+            return "At your service. As you wish, naturally";
         }
-        else {
         return "Right.And with that I shall Retire";
         }
     }
-}
